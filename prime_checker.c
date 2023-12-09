3b68a07849c97cdc0a08cc13d7a8289dfa6b0fbb
Daniel Aspect <daniel_aspect@proton.me>
2023-12-09 20:02:32 -0300

    fixed true and false returns

--- prime_checker.c
+++ prime_checker.c
@@ -4,16 +4,15 @@
 
 int isPrime(int number) {
   if (number < 2 || (number % 2 == 0 && number != 2)) {
-    return 1;
+    return 0;
   }
 
-  int i;
   int numberSquareRoot = floor(sqrt(number));
-  for (i = 3; i <= numberSquareRoot; i += 2) {
+  for (int i = 3; i <= numberSquareRoot; i += 2) {
     if (number % i == 0) {
-      return 1;
+      return 0;
     }
   }
 
-  return 0;
+  return 1;
 }
